a947821f0415757e90d98ae0fda803cf48794817
mingli <985579@cslab-m105-305.cs.mum.edu>
2016-12-02 12:20:22 -0600

    add MergeSortedString

---
+++ FPP-HomeWork/src/com/ming/HomeWorkLesson4/MergeSortedString.java
@@ -0,0 +1,53 @@
+package com.ming.HomeWorkLesson4;
+
+/**
+ * 
+ * @author  Ming Li  
+ * @Student No  985579
+ * @date Dec 2,2016
+ * 
+ * @description:    Solution for Quesetion 1 
+ *
+ */
+public class MergeSortedString {
+
+	public MergeSortedString() {
+		
+	}
+	
+	public static String getMergeSortedString(String string1,String string2) {
+		//System.out.println("string1:"+string1+"|string2:"+string2);
+		if (string1.length() == 0 && string2.length() == 0 ){
+			return "";
+		}
+		if (string1.length() == 0){
+			return string2.substring(0, 1)+getMergeSortedString("",string2.substring(1));
+		}
+		if (string2.length() == 0){
+			return string1.substring(0, 1)+getMergeSortedString(string1.substring(1),"");
+		}
+		if (string1.charAt(0) <= string2.charAt(0)){
+			return string1.substring(0, 1)+string2.substring(0, 1)+getMergeSortedString(string1.substring(1),string2.substring(1));
+		}else{
+			return string2.substring(0, 1)+string1.substring(0, 1)+getMergeSortedString(string1.substring(1),string2.substring(1)); 
+		}
+	}
+	
+	
+	
+/*
+Find the solutions for the given problem using recursive approach.
+1. Merge the two sorted string into a single sorted string.
+Example : String1 = “ ace ”
+ String2 = “ bdf ”
+ Output : “ abcdef ”
+*/
+
+	public static void main(String[] args) {
+		String string1 = "acef";
+		String string2 = "bdg";
+		String result = getMergeSortedString(string1,string2);
+		System.out.println(result);
+	}
+
+}
---
+++ FPP-HomeWork/src/com/ming/HomeWorkLesson4/RecursionMinimum.java
@@ -0,0 +1,36 @@
+package com.ming.HomeWorkLesson4;
+
+
+/**
+ * 
+ * @author  Ming Li  
+ * @Student No  985579
+ * @date Dec 2,2016
+ * 
+ * @description:    Solution for Quesetion 1 
+ *
+ */
+
+public class RecursionMinimum {
+
+	public RecursionMinimum() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void getRecursionMinimum(){
+		char result;
+		//return result;
+	}
+	/*
+2. Using recursion to find the minimum character in a given string input.
+If your input is “akel” in return ‘a’
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String string = "akel";
+		string = "Move disk 1 from A to C";
+		//()
+	}
+
+}
