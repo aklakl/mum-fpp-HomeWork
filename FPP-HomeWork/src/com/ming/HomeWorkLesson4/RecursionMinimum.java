f1d9e71cdaa2f8950c12b142972fbda8ebe277ea
mingli <985579@cslab-m105-305.cs.mum.edu>
2016-12-02 13:44:38 -0600

    complete homeworklesson4 question2

--- FPP-HomeWork/src/com/ming/HomeWorkLesson4/RecursionMinimum.java
+++ FPP-HomeWork/src/com/ming/HomeWorkLesson4/RecursionMinimum.java
@@ -7,7 +7,7 @@ package com.ming.HomeWorkLesson4;
  * @Student No  985579
  * @date Dec 2,2016
  * 
- * @description:    Solution for Quesetion 1 
+ * @description:    Solution for Quesetion 2 
  *
  */
 
@@ -17,20 +17,30 @@ public class RecursionMinimum {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public static void getRecursionMinimum(){
-		char result;
-		//return result;
-	}
 	/*
 2. Using recursion to find the minimum character in a given string input.
 If your input is “akel” in return ‘a’
 	 */
+	public static char getRecursionMinimum(String target){
+		if (target.length()==1){
+			return  target.charAt(0);
+		}
+		if (target.charAt(0) < target.charAt(1)){
+			return getRecursionMinimum(target.substring(0, 1) + target.substring(2));
+		}else{
+			return getRecursionMinimum(target.substring(2));
+		}
+	}
+	
+	
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String string = "akel";
 		string = "Move disk 1 from A to C";
-		//()
+		char minimum = getRecursionMinimum(string);
+		System.out.println("The string: \""+string+"\" has minimum value is "+minimum);
 	}
 
 }
