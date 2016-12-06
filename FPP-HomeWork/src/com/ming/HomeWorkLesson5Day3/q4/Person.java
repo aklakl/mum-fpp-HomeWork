2874e0b16ed562dde11e9250fbc8014d0ba3332d
mingli <985579@cslab-m105-305.cs.mum.edu>
2016-12-06 14:24:24 -0600

    Revert "completed Solution for com.ming.HomeWorkLesson5Day3 Quesetion 3 & 4"
    This reverts commit 4d6dcecfa246b908edace5c49afaba5cf59c62b8.

--- FPP-HomeWork/src/com/ming/HomeWorkLesson5Day3/q4/Person.java
+++ FPP-HomeWork/src/com/ming/HomeWorkLesson5Day3/q4/Person.java
@@ -1,7 +1,5 @@
 package com.ming.HomeWorkLesson5Day3.q4;
 
-import java.lang.reflect.Field;
-
 /**
  * 
  * @author  Ming Li  
@@ -13,64 +11,10 @@ import java.lang.reflect.Field;
  */
 import com.ming.HomeWorkLesson5Day3.q3.Computer;
 
+class Person {
 
-class Person implements Cloneable {
+	  String name;           
+	  Computer computer; // Refer from the Problem 3. 
 
-	String name;           
-	Computer computer; // Refer from the Problem 3. 
-
- 	public Object shadowClone() throws CloneNotSupportedException {
- 		Person clone = (Person)super.clone();
-	    return clone;
-	}
- 	
- 	public Object deepClone() throws CloneNotSupportedException {
- 		Person clone = (Person)super.clone();
- 		//Need to clone the Address reference variable too
- 		clone.computer = (Computer)this.computer.clone();
-	    return clone;
-	}
-	
-	public Person(String name, Computer computer) {
-		super();
-		this.name = name;
-		this.computer = computer;
-	}
-	
-	@Override
-	public String toString(){
-		return this.name+" "+this.computer.toString();
-	}
-	
-	//a. Apply clone() method and write the implementation to understand the concepts of shallow and deep copy. 
-	public static void main(String[] args) {
-		Computer computer1 = new Computer("computer1", "processor", 128, 1024, 24);
-		Computer computer2 = new Computer("IBM", "intel", 128, 1024, 24);
-		Person person1 = new Person("Jonh",computer1);
-		Person person2 = new Person("Ming",computer2);
-		Person person11 = null ,person22 = null;
-		try {
-			person11 = (Person)person1.shadowClone();
-			person22 = (Person)person2.deepClone();
-		} catch (CloneNotSupportedException e) {
-			e.printStackTrace();
-		}
-		System.out.println("person11 shadowClone person1:");
-		System.out.println("person22 deepClone: person2:");
-		System.out.println("person1:"+person1.toString());
-		System.out.println("person2:"+person2.toString());
-		System.out.println("person11:"+person11.toString());
-		System.out.println("person22:"+person22.toString());
-		System.out.println("===============================");
-		System.out.println("modify person1.computer.Manufacturer and modify person2.computer.Manufacturer");
-		person1.computer.setManufacturer(person1.computer.getManufacturer()+ " changed flag" );
-		person2.computer.setManufacturer(person2.computer.getManufacturer()+ " changed flag" );
-		System.out.println("person1:"+person1.toString());
-		System.out.println("person2:"+person2.toString());
-		System.out.println("person11:"+person11.toString());
-		System.out.println("person22:"+person22.toString());
-		
-	}
-	
-	
+	  //a. Apply clone() method and write the implementation to understand the concepts of shallow and deep copy. 
 }
