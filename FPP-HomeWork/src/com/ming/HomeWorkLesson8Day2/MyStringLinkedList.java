2a1c41ca68cbe31ecf6edd26734b2b30cc2dfddf
mingli <985579@cslab-m105-305.cs.mum.edu>
2016-12-13 23:46:10 -0600

    complete  Solution for com.ming.HomeWorkLesson8Day2 Quesetion 1

--- FPP-HomeWork/src/com/ming/HomeWorkLesson8Day2/MyStringLinkedList.java
+++ FPP-HomeWork/src/com/ming/HomeWorkLesson8Day2/MyStringLinkedList.java
@@ -32,13 +32,30 @@ public class MyStringLinkedList {
 
 	// Implement the code
 	public void addLast(String item) {
-
-		size++;
+		Node lastNode = this.findLast();
+		postAddNode(lastNode,item);
+		//lastNode;
 	}
 
 	// implement the code
 	public void postAddNode(Node n, String value) {
-
+		//System.out.println("postAddNode  Node:"+n.toString()+"|value:"+value);
+		Node newNode = new Node(null,value,null);
+		newNode.next = n.next;
+		n.next = newNode;
+		if (n.next !=null) {//deal with common node
+			newNode.previous = n ;
+		};
+		/*
+		if (n.next !=null) {//deal with common node
+			newNode.next = n.next;
+			n.next = newNode;
+			newNode.previous = n ;
+		}else{//deal with lastnode
+			newNode.next = n.next;
+			n.next = newNode;
+		}
+		*/
 		size++;
 	}
 
@@ -49,16 +66,74 @@ public class MyStringLinkedList {
 	}
 
 	// implement code
-	public boolean isEmpty() {
-		
-		return false;
+	public boolean isEmpty(){
+		if  (this.header ==  null)
+			return true;
+		else
+			return false;	
 	}
-
+	
+	// implement code
+	public Node getFirst() {
+		return header;
+	}
+	
+	// implement code
+	public Node getLast() {
+		int sizeNew = 0;
+		if (header == null)
+			return null;
+		else {
+			Node temp = header;
+			while (temp.next != null){
+				//System.out.println("header:"+header+"|temp:"+temp);
+				temp = temp.next;
+				sizeNew++;
+			}
+			//System.out.println("findLast method cal sizeNew:"+sizeNew+"|size"+size);
+			if (sizeNew != size){
+				size = sizeNew;
+			}
+			return temp;
+		}
+	}
+	
+	// implement code
+	public void removeFirst() {
+		if (header != null){
+			Node temp = header;
+			temp.next.previous = null;
+			header = temp.next;
+			size--;
+		}
+	}
+	
+	// implement code
+	public void removeLast() {
+		Node n = getLast();
+		this.deleteNode(n);
+		size--;
+	}
+	
+	// implement code
+	public void print(Node n) {
+		//test for public void print(Node n) {} //Write a recursive print method to display the elements in the list. Start with the First Node(Head) 
+		if (n == null) return;
+		if (n.next == null) {
+			System.out.println(n.value);
+			return ;
+		}
+		System.out.println(n.value);
+		this.print(n.next);
+	}
+	
+	
 	public void preAddNode(Node n, String value) {
 		Node newNode;
 		if (n == null) {
 			// List is empty
 			newNode = new Node(null, value, null);
+			size++;
 		} else if (n.previous == null) {
 			// n is the first node
 			addFront(value);
@@ -66,8 +141,9 @@ public class MyStringLinkedList {
 			newNode = new Node(n.previous, value, n);
 			n.previous.next = newNode;
 			n.previous = newNode;
+			size++;
 		}
-		size++;
+		
 	}
 
 	public Node findLast() {
@@ -182,47 +258,78 @@ public class MyStringLinkedList {
 			return value;
 		}
 
-
 	}
 
 	
-	
+	public static void myTest() {
+		MyStringLinkedList mySL = new MyStringLinkedList();
+		MyStringLinkedList temp = mySL;
+		mySL.addFront("Pen Pineapple Apple Pen");
+		System.out.println("1 mySL: "+mySL+"|temp:"+temp);
+		mySL.addFront("11111n");
+		System.out.println("2 mySL: "+mySL+"|temp:"+temp);
+		temp = null;
+		System.out.println("3 mySL: "+mySL+"|temp:"+temp);
+	}
 	
 	public static void main(String[] args) {
-		preTest();
+		//preTest();
+		//myTest();
 		MyStringLinkedList mySL = new MyStringLinkedList();
+		System.out.println("test for MyStringLinkedList isEmpty print:"+mySL.isEmpty());
 		mySL.addFront("Pen Pineapple Apple Pen");
-		System.out.println(mySL);
+		mySL.addFront("F");
+		mySL.addFront("E");
+		mySL.addFront("D");
+		mySL.addFront("C");
+		mySL.addFront("B");
+		mySL.addFront("A");
+		System.out.println("first print MyStringLinkedList: "+mySL);
 		
 		
 		//test for  public void addLast(String item) { }
-		mySL.addLast("Carrot Cake");
+		mySL.addLast("addLast");
+		mySL.addLast("Ming Li");
+		System.out.println("test for addLast print: "+mySL);
+		
 		
 		//test for public void postAddNode(Node n, String value){} 
+		mySL.postAddNode(mySL.findItem("Pen Pineapple Apple Pen"),"postAddNode");
+		System.out.println("test for postAddNode print: "+mySL);
 		
 		
 		//test for public int size(){ } 
 		System.out.println("MyStringLinkedList size is "+mySL.Size());
 		
-		//test for public boolean isEmpty(){}   
+		
+		//test for public boolean isEmpty(){}
+		System.out.println("test for MyStringLinkedList isEmpty print:"+mySL.isEmpty());
 		
 		
 		//test for public object getFirst(){}
+		System.out.println("test for MyStringLinkedList getFirst print:"+mySL.getFirst());
 		
 		
-		//test for public object getLast(){} 
+		//test for public object getLast(){}
+		System.out.println("test for MyStringLinkedList getLast print:"+mySL.getLast());
 		
 		
-		//test for public void removeFirst(){} 
+		//test for public void removeFirst(){}
+		System.out.println("test for MyStringLinkedList before removeFirst print:"+mySL);
+		mySL.removeFirst();
+		System.out.println("test for MyStringLinkedList after  removeFirst print:"+mySL);
 		
 		
-		//test for public void removeLast(){} 
+		//test for public void removeLast(){}
+		System.out.println("before MyStringLinkedList removeLast print:"+mySL);
+		mySL.removeLast();
+		System.out.println( "after MyStringLinkedList removeLast print:"+mySL);
 		
 		
-		//test for public void  print(Node n) {} //Write a recursive print method to display
-		
-		
-		//the elements in the list. Start with the First Node(Head) 
+		//test for public void print(Node n) {} //Write a recursive print method to display the elements in the list. Start with the First Node(Head) 
+		System.out.println("test for MyStringLinkedList print(Node n) print below");
+		//mySL.print(mySL.findItem("Ming Li"));
+		mySL.print(mySL.findItem("B"));
 		
 		
 		
