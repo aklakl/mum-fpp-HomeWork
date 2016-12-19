a368a38a588d176946bd1704fa5d40a96c8ada8f
mingli <985579@cslab-m105-305.cs.mum.edu>
2016-12-19 11:31:29 -0600

    push some last practice code

--- FPP-HomeWork/src/com/ming/HomeWorkLesson11/HashTableApp.java
+++ FPP-HomeWork/src/com/ming/HomeWorkLesson11/HashTableApp.java
@@ -13,40 +13,54 @@ package com.ming.HomeWorkLesson11;
 import java.io.*;
 import java.util.Scanner;
 
-////////////////////////////////////////////////////////////////
-class DataItem { // (could have more data)
-	private int iData; // data item (key)
-	private String value;
+class PersonItem {
+	
+	private String mail_id;  	//Person E_Mail-id-Key 
+	
+	private String name;  		// Name of the person } 
 
-	// --------------------------------------------------------------
-	public DataItem(int ii, String v) // constructor
-	{
-		iData = ii;
-		value = v;
+	/**
+	 * @param mail_id
+	 * @param name
+	 */
+	public PersonItem(String mail_id, String name) {
+		super();
+		this.mail_id = mail_id;
+		this.name = name;
 	}
 
-	// --------------------------------------------------------------
-	public int getKey() {
-		return iData;
+	public String getMail_id() {
+		return mail_id;
 	}
 
-	public String getValue() {
-		return value;
+	public void setMail_id(String mail_id) {
+		this.mail_id = mail_id;
 	}
-	// --------------------------------------------------------------
-} // end class DataItem
-	////////////////////////////////////////////////////////////////
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
+
+
+ 
 
 class HashTable {
-	private DataItem[] hashArray; // array holds hash table
+	private PersonItem[] hashArray; // array holds hash table
 	private int arraySize;
-	private DataItem nonItem; // for deleted items
+	private PersonItem nonItem; // for deleted items
 	// -------------------------------------------------------------
 
 	public HashTable(int size) // constructor
 	{
 		arraySize = size;
-		hashArray = new DataItem[arraySize];
+		hashArray = new PersonItem[arraySize];
 		nonItem = null; // deleted item key is -1
 	}
 
@@ -55,7 +69,7 @@ class HashTable {
 		System.out.println("Table: ");
 		for (int j = 0; j < arraySize; j++) {
 			if (hashArray[j] != null)
-				System.out.println("Index : " + j + " " + hashArray[j].getKey() + " " + hashArray[j].getValue());
+				System.out.println("Index : " + j + " " + hashArray[j].getMail_id() + " " + hashArray[j].getName());
 			else
 				System.out.println("Index : " + j + " " + "**");
 		}
@@ -63,12 +77,12 @@ class HashTable {
 	}
 
 	// -------------------------------------------------------------
-	public int hashFunc(int key) {
-		return key % arraySize; // hash function
+	public int hashFunc(String key) {
+		return key.hashCode() % arraySize; // hash function
 	}
 
 	// -------------------------------------------------------------
-	public void insert(int key, String v) // insert a DataItem
+	public void insert(String key, String v) // insert a DataItem
 	// (assumes table not full)
 	{
 		// extract key
@@ -76,23 +90,23 @@ class HashTable {
 										// until empty cell
 		// an index have some data and not match with existing key, it find next
 		// vacant position
-		while (hashArray[hashVal] != null && hashArray[hashVal].getKey() != key) {
+		while (hashArray[hashVal] != null && !hashArray[hashVal].getMail_id().equals(key)  ) {
 			++hashVal; // go to next cell
 			hashVal %= arraySize; // wraparound if necessary
 		}
-		DataItem obj = new DataItem(key, v);
+		PersonItem obj = new PersonItem(key, v);
 		hashArray[hashVal] = obj; // insert item
 	} // end insert()
 		// -------------------------------------------------------------
 
-	public DataItem delete(int key) // delete a DataItem
+	public PersonItem delete(String key) // delete a DataItem
 	{
 		int hashVal = hashFunc(key); // hash the key
 
 		while (hashArray[hashVal] != null) // until empty cell,
 		{ // found the key?
-			if (hashArray[hashVal].getKey() == key) {
-				DataItem temp = hashArray[hashVal]; // save item
+			if (hashArray[hashVal].getMail_id().equals(key)) {
+				PersonItem temp = hashArray[hashVal]; // save item
 				hashArray[hashVal] = nonItem; // delete item
 				return temp; // return item
 			}
@@ -103,13 +117,13 @@ class HashTable {
 	} // end delete()
 		// -------------------------------------------------------------
 
-	public boolean find(int key) // find item with key
+	public boolean find(String key) // find item with key
 	{
 		int hashVal = hashFunc(key); // hash the key
 
 		while (hashArray[hashVal] != null) // until empty cell,
 		{ // found the key?
-			if (hashArray[hashVal].getKey() == key)
+			if (hashArray[hashVal].getMail_id().equals(key))
 				return true; // yes, return item
 			++hashVal; // go to next cell
 			hashVal %= arraySize; // wraparound if necessary
@@ -120,9 +134,11 @@ class HashTable {
 } // end class HashTable
 	////////////////////////////////////////////////////////////////
 
- class HashTableApp {
-	public static void main(String[] args) throws IOException {
-		int aKey;
+class HashTableApp {
+	 
+	/*
+	public static void preTest(){
+		String aKey;
 		String value;
 
 		Scanner in = new Scanner(System.in);
@@ -131,10 +147,10 @@ class HashTable {
 
 		HashTable theHashTable = new HashTable(size);
 
-		theHashTable.insert(1, "FPP");
-		theHashTable.insert(5, "Java");
-		theHashTable.insert(7, "SE");
-		theHashTable.insert(10, "C#");
+		theHashTable.insert("1", "FPP");
+		theHashTable.insert("5", "Java");
+		theHashTable.insert("7", "SE");
+		theHashTable.insert("10", "C#");
 
 		while (true) // interact with user
 		{
@@ -147,24 +163,78 @@ class HashTable {
 				break;
 			case 'i':
 				System.out.print("Enter key and value to insert: ");
-				aKey = in.nextInt();
+				aKey = in.next();
 				value = in.next();
 				theHashTable.insert(aKey, value);
 				break;
 			case 'd':
 				System.out.print("Enter key value to delete: ");
-				aKey = in.nextInt();
+				aKey = in.next();
 				theHashTable.delete(aKey);
 				break;
 			case 'f':
 				System.out.print("Enter key value to find: ");
-				aKey = in.nextInt();
+				aKey = in.next();
 				System.out.println("Key Found status" + theHashTable.find(aKey));
 				break;
 			default:
 				System.out.print("Invalid entry\n");
 			} // end switch
 		} // end while
+	}
+	*/
+	
+	public static void preTestPerson(){
+		String aKey;
+		String value;
+
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter size of hash table: ");
+		int size = in.nextInt();
+
+		HashTable theHashTable = new HashTable(size);
+		
+		for(int i = 0 ; i<10 ; i++){
+			theHashTable.insert("key"+i,"value"+i );	
+		}
+		
+		while (true) // interact with user
+		{
+			System.out.print("Enter first letter of ");
+			System.out.print("show, insert, delete, or find: ");
+			char choice = in.next().charAt(0); // Read a Char from the console
+			switch (choice) {
+			case 's':
+				theHashTable.displayTable();
+				break;
+			case 'i':
+				System.out.print("Enter key and value to insert: ");
+				aKey = in.next();
+				value = in.next();
+				theHashTable.insert(aKey, value);
+				break;
+			case 'd':
+				System.out.print("Enter key value to delete: ");
+				aKey = in.next();
+				theHashTable.delete(aKey);
+				break;
+			case 'f':
+				System.out.print("Enter key value to find: ");
+				aKey = in.next();
+				System.out.println("Key Found status" + theHashTable.find(aKey));
+				break;
+			default:
+				System.out.print("Invalid entry\n");
+			} // end switch
+		} // end while
+	}
+	
+	public static void main(String[] args) throws IOException {
+		//preTest();
+		preTestPerson();
+		
+		
+		
 	} // end main()
 		// --------------------------------------------------------------
 } // end class HashTableApp
